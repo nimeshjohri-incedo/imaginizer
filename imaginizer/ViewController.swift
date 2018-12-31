78aad6c46eda236f0ba9f01d04327ece0c3d58f0
Nimesh Johri <nimesh.johri@incedoinc.com>
2018-12-31 14:10:38 +0530

    Setup project

---
+++ imaginizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  imaginizer
+//
+//  Created by Nimesh Johri on 12/27/18.
+//  Copyright © 2018 Nimesh Johri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
